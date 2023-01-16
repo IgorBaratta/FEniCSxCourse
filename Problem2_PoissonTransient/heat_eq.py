96c941f0e7b8d28c0cd5cf94df54eea6fcd53b20
IgorBaratta <igorbaratta@gmail.com>
2023-01-16 18:52:17 +0000

    minor update

--- Problem2_PoissonTransient/heat_eq.py
+++ Problem2_PoissonTransient/heat_eq.py
@@ -91,58 +91,46 @@
 # of tetrahedral or hexahedral elements. We start by creating a unit square:
 
 # +
+# Convenience functions for plotting on jupyter notebooks
+from utils import plot_mesh, plot_function, create_gif
+from dolfinx import geometry
 from dolfinx import io
-from dolfinx.mesh import locate_entities
+from dolfinx.mesh import locate_entities, exterior_facet_indices
 import numpy
 from petsc4py import PETSc
-from dolfinx.mesh import exterior_facet_indices
-from ufl import inner, grad, dx
-from ufl import TestFunction, TrialFunction
-from matplotlib import pyplot
+from ufl import TestFunction, TrialFunction, inner, grad, dx
 from dolfinx import fem
-import pyvista
 import IPython
-from dolfinx import plot
 from dolfinx.mesh import create_unit_square
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
-mesh = create_unit_square(comm, 10, 10)
+mesh = create_unit_square(comm, 20, 20)
 # -
 
 # ### Visualizing mesh
 
+# We have provided a few convenience functions for plotting on
+# jupyter notebooks.
+
 # +
-
-
-def plot_mesh(mesh, filename="file.html"):
-    pyvista.start_xvfb()
-    grid = pyvista.UnstructuredGrid(*plot.create_vtk_mesh(mesh))
-    plotter = pyvista.Plotter(notebook=True)
-    plotter.add_mesh(grid, show_edges=True)
-    plotter.camera.zoom(2.0)
-    plotter.view_xy()
-    plotter.export_html(filename, backend="pythreejs")
-    plotter.close()
-
-
-plot_mesh(mesh, "mesh.html")
+plot_mesh(mesh, filename="mesh.html")
 IPython.display.HTML(filename="mesh.html")
 # -
 
 # ### Handling time-dependent functions expressions
 
 # +
-
 # Define our 5 constants c0, c1, ..., c4
-c0 = fem.Constant(mesh, -3.0)
+c0 = fem.Constant(mesh, 0.0)
 c1 = fem.Constant(mesh, 2.0)
-c2 = fem.Constant(mesh, -1.0)
-c3 = fem.Constant(mesh, 2.0)
-c4 = fem.Constant(mesh, 0.3)
+c2 = fem.Constant(mesh, 0.0)
+c3 = fem.Constant(mesh, 0.0)
+c4 = fem.Constant(mesh, 0.01)
 dt = fem.Constant(mesh, 0.1)
 # -
 
+# We can define the source term as a Constant:
 # $$
 # \begin{align*}
 #   f = c_4 - 2(c_0 + c_3)
@@ -153,53 +141,25 @@ dt = fem.Constant(mesh, 0.1)
 f = c4 - 2*(c0 + c3)
 # -
 
+# For T, T0 and T_D we first define an expression and then we interpolate
+# it into the corresponding function.
+
 # +
-
-
-def exact_solution(t):
+def expression(t):
     return lambda x: c0*x[0]**2 + c1*x[0] + c2*x[1]**2 + c3*x[1] + c4*t
 
 
 V = fem.FunctionSpace(mesh, ("Lagrange", 1))
 T0 = fem.Function(V)
-T0.interpolate(exact_solution(0))
-# -
+T0.interpolate(expression(t=0))
 
-
-# ### Visualizing a function
-
-# +
-
-
-def plot_function(uh, filename):
-    pyvista.start_xvfb(0.5)
-    plotter = pyvista.Plotter(notebook=False, off_screen=True)
-    if "gif" in filename:
-        plotter.open_gif(filename)
-
-    V = uh.function_space
-    topology, cells, geometry = plot.create_vtk_mesh(V)
-    grid = pyvista.UnstructuredGrid(topology, cells, geometry)
-    grid.point_data["uh"] = uh.x.array
-    viridis = pyplot.cm.get_cmap("viridis", 25)
-    # sargs = dict(title_font_size=25, label_font_size=20, fmt="%.2e", color="black",
-    #              position_x=0.1, position_y=0.8, width=0.8, height=0.1)
-    plotter.add_mesh(grid, show_edges=True, lighting=False, cmap=viridis)
-    plotter.camera.zoom(2.0)
-    plotter.view_xy()
-
-    if "html" in filename:
-        plotter.export_html(filename, backend="pythreejs")
-
-    return plotter
-
-
-plot_function(T0, "T0.html")
+plot_function(T0, filename="T0.html")
 IPython.display.HTML(filename="T0.html")
 # -
 
 
 # ## Setting up a variational problem
+
 # +
 V = fem.FunctionSpace(mesh, ("Lagrange", 1))
 T = TrialFunction(V)
@@ -209,18 +169,16 @@ T0 = fem.Function(V)
 
 
 # The variational form can be written in UFL syntax:
-
 # $$
 # \begin{align*}
 #   a(T, v) = \int_{\Omega} T v \,dx + \mu \Delta t \int_{\Omega}{\nabla T \cdot \nabla v}\,dx
 # \end{align*}
 # $$
 
-
 # +
 mu = fem.Constant(mesh, 1.0)
 a = inner(T, v) * dx + dt * mu * inner(grad(T), grad(v)) * dx
-a = fem.form(a)
+a = fem.form(a)  # JIT compilation
 # -
 
 # $$
@@ -243,19 +201,24 @@ L = dt * inner(f, v) * dx + inner(T0, v) * dx
 # DOLFINx also has convenience functions to obtain a list of all boundary facets.
 
 # +
-
 T_D = fem.Function(V)
-T_D.interpolate(exact_solution(0))
+T_D.interpolate(expression(0))
 
-tdim = mesh.topology.dim
-mesh.topology.create_connectivity(tdim - 1, tdim)
-bndry_facets = exterior_facet_indices(mesh.topology)
-bndry_dofs = fem.locate_dofs_topological(V, tdim - 1, bndry_facets)
-bcs = [fem.dirichletbc(T_D, bndry_dofs)]
+
+def create_dirichletbc(mesh, u):
+    tdim = mesh.topology.dim
+    mesh.topology.create_connectivity(tdim - 1, tdim)
+    bndry_facets = exterior_facet_indices(mesh.topology)
+    bndry_dofs = fem.locate_dofs_topological(V, tdim - 1, bndry_facets)
+    bcs = [fem.dirichletbc(u, bndry_dofs)]
+    return bcs
+
+
+bcs = create_dirichletbc(mesh, T_D)
 # -
 
 # ### Setting up a time dependent solver
-
+#
 # As the left hand side of our problem(the matrix) is time independent, we would like avoid re-assembling it at every time step.
 # We assemble the matrix once outside the temporal loop.
 
@@ -273,10 +236,10 @@ L = fem.form(L)  # JIT compilation
 # -
 
 
-# # We next create the PETSc KSP(Krylov subspace method) solver, and set it to solve using an
-# # [algebraic multigrid method](https: // hypre.readthedocs.io/en/latest/solvers-boomeramg.html).
-# +
+# We next create the PETSc KSP(Krylov subspace method) solver, and set it to solve using an
+# [algebraic multigrid method](https: // hypre.readthedocs.io/en/latest/solvers-boomeramg.html).
 
+# +
 solver = PETSc.KSP().create(comm)
 solver.setOperators(A)
 solver.setType(PETSc.KSP.Type.CG)
@@ -295,7 +258,7 @@ pc.setHYPREType("boomeramg")
 
 # +
 T = fem.Function(V)
-plotter = plot_function(T, "T.gif")
+plotter = create_gif(T, "T.gif")
 plotter.write_frame()
 # -
 
@@ -307,105 +270,173 @@ plotter.write_frame()
 # 2. Reassemble the right hand side vector `b`
 # 3. Apply boundary conditions to `b`
 # 4. Solve linear problem `AT = b`
-# 5. Update previous time step, `T0 = T`
+# 5. Update current solution, `T0 = T`
 
-# # +
-# from dolfinx.la import ScatterMode
+# +
+from dolfinx.la import ScatterMode
 
-# t = 0
-# t_max = 2
-# while t < t_max:
-#     t += dt.value
-#     print(f"t = {t}")
+t = 0
+t_max = 0
+while t < t_max:
+    t += dt.value
+    print(f"t = {t} s")
 
-#     # Update boundary condition
-#     T_D.interpolate(exact_solution(t))
+    # Update boundary condition
+    T_D.interpolate(expression(t))
 
-#     # Assemble RHS
-#     b.x.array[:] = 0
-#     fem.petsc.assemble_vector(b.vector, L)
+    # Assemble RHS
+    b.x.array[:] = 0
+    fem.petsc.assemble_vector(b.vector, L)
 
-#     # Apply boundary condition
-#     fem.petsc.apply_lifting(b.vector, [a], [bcs])
-#     b.x.scatter_reverse(ScatterMode.add)
-#     fem.petsc.set_bc(b.vector, bcs)
+    # Apply boundary condition
+    fem.petsc.apply_lifting(b.vector, [a], [bcs])
+    b.x.scatter_reverse(ScatterMode.add)
+    fem.petsc.set_bc(b.vector, bcs)
 
-#     # Solve linear problem
-#     solver.solve(b.vector, T.vector)
-#     T.x.scatter_forward()
+    # Solve linear problem
+    T.x.array[:] = 0
+    solver.solve(b.vector, T.vector)
+    T.x.scatter_forward()
 
-#     # Update un
-#     T0.x.array[:] = T.x.array
+    # Update un
+    T0.x.array[:] = T.x.array
 
-#     # Update plotter
-#     plotter.update_scalars(T.x.array, render=False)
-#     plotter.write_frame()
+    # Update plotter
+    plotter.update_scalars(T.x.array, render=False)
+    plotter.write_frame()
 
-# IPython.display.Image(filename="T.gif")
-# # -
 
+IPython.display.Image(filename="T.gif")
+
+# Compute error norm
+M = fem.form((T_D - T)**2 * dx)
+error_norm = comm.allreduce(fem.assemble_scalar(M), op=MPI.SUM)
+print(error_norm)
+# -
+
+# What's the temperature at an arbitrary point p?
+
+# +
+# Given an arbitrary point "p"
+p = numpy.array([0.456, 0.385, 0.0], dtype=numpy.float64)
+
+# We first compute the cells that it belongs to
+bb_tree = geometry.BoundingBoxTree(mesh, mesh.topology.dim)
+cell_candidates = geometry.compute_collisions(bb_tree, p)
+cells = geometry.compute_colliding_cells(mesh, cell_candidates, p)
+
+# Given a list of cells it's easy and "efficient" to compute T(p)
+value = T.eval(p, cells)
+print(f"Temperature at point {p} is {value[0]}")
+# -
 
 # ## Homework 1
+# In this homework we are going to tackle a more physically realistic problem.
+# Given the temperature outside a room
+
+# The strong form of our problem reads:
+# $$
+# \begin{align*}
+# \frac{\partial T}{\partial t} &= \nabla \cdot (\alpha  \nabla T) & & \text{in } \, \Omega, \\
+# T(\boldsymbol{x}, t) &= T_R + T_a \cdot sin(\omega t) & & \,\partial\Omega_\text{D}, \\
+# T(\boldsymbol{x}, t=0) &= T_R & & \text{in } \, \Omega,
+# \end{align*}
+# $$
+# Where $\alpha$ is the thermal diffusivity of the medium:
+# $$
+# \begin{align*}
+# \alpha = \frac{k}{\rho C_p}
+# \end{align*}
+# $$
+# $k$ is the thermal conductivity, $\rho$ is the density and $C_p$ is the is specific heat capacity.
+
+# Examples of thermal diffusivity for three different materials:
+# $$\alpha_{air} = 19 \cdot 10^{-6} \, m^2/s$$
+# $$\alpha_{brick} = 0.27 \cdot 10^{-6} \, m^2/s$$
+# $$\alpha_{wood} = 0.082 \cdot 10^{-6} \, m^2/s$$
+# $$\alpha_{steel} = 11.72 \cdot 10^{-6} \, m^2/s$$
+
+# Given a 1 squared meter room, 0.1 m thick walls, and with outside 
+# temperature given by $-5 + 15 \cdot sin(\omega t)$ which material 
+# should be used for the wall such that the temperature at the center 
+# of the room is never below $15^oC$?
 
 
+# Complete the following code with the thermal diffusivity of the air and
+# the wall and visualize the material distribution:
+
+# +
 def wall(x):
-    bottom = x[0] <= 0.1 + 1e-10
-    top = x[0] >= 0.9 - 1e-10
-    left = x[1] <= 0.1 + 1e-10
-    right = x[1] >= 0.9 - 1e-10
-    vertical = numpy.logical_or(bottom, top)
-    horizontal = numpy.logical_or(left, right)
-    return numpy.logical_or(vertical, horizontal)
+    vertical = (x[0] <= 0.1) | (x[0] >= 0.9)
+    horizontal = (x[1] <= 0.1) | (x[1] >= 0.9)
+    return horizontal | vertical
 
+
+comm = MPI.COMM_WORLD
+mesh = create_unit_square(comm, 20, 20)
 
 cells = locate_entities(mesh, mesh.topology.dim, wall)
 DG = fem.FunctionSpace(mesh, ("DG", 0))
 
 # Thermal diffusivity
 alpha = fem.Function(DG)
-alpha.x.array[:] = 0.0082
-alpha.x.array[cells] = 0.0082
+# alpha.x.array[:] = alpha_air
+# alpha.x.array[cells] = alpha_wall
 
+# Solution - 
+alpha.x.array[:] = 19e-6
+alpha.x.array[cells] = 0.082e-6
 
-with io.XDMFFile(MPI.COMM_WORLD, "inclusions.xdmf", "w") as xdmf:
-    xdmf.write_mesh(mesh)
-    xdmf.write_function(alpha)
+plot_mesh(mesh, cell_values=alpha, filename="alpha.html")
+IPython.display.HTML(filename="alpha.html")
+# -
 
-period = 3600
-
+## Task 2
+# +
+# 24 hours in a day, 60 min in an hour, 60 seconds a minute
+period = 60*60*24
 
 def outside_temperature(t):
     omega = numpy.pi/period
-    temperature = 25 * numpy.sin(omega*t)
+    t = t % period
+    temperature = 15 * numpy.sin(omega*t) - 5
     print(f"Outside temperature = {temperature}")
     return lambda x: numpy.full_like(x[0], temperature)
+# -
 
-
+# +
 def thermal_solver(alpha):
-    V = fem.FunctionSpace(mesh, ("Lagrange", 1))
+    mesh = alpha.function_space.mesh
 
+    # Create function space
+    V = fem.FunctionSpace(mesh, ("Lagrange", 1))
+    
+    # Define trial and test functions
     T = TrialFunction(V)
     v = TestFunction(V)
+
+    # Initial condition
     T0 = fem.Function(V)
-    T0.interpolate(lambda x: numpy.full_like(x[0], 0))
+    T0.interpolate(lambda x: numpy.full_like(x[0], 12))
 
-    dt = fem.Constant(mesh, 5.0)
+    # Time step
+    dt = fem.Constant(mesh, 900.0)
 
+    # Bilinear form
     a = inner(T, v) * dx + dt * alpha * inner(grad(T), grad(v)) * dx
     a = fem.form(a)
-    
+
+    # Linear form
     L = inner(T0, v) * dx
     L = fem.form(L)  # JIT compilation
 
 
+    # Define dirichlet bc at t=0
     T_D = fem.Function(V)
     T_D.interpolate(outside_temperature(0))
-    tdim = mesh.topology.dim
-    mesh.topology.create_connectivity(tdim - 1, tdim)
-    bndry_facets = exterior_facet_indices(mesh.topology)
-    bndry_dofs = fem.locate_dofs_topological(V, tdim - 1, bndry_facets)
-    bcs = [fem.dirichletbc(T_D, bndry_dofs)]
+    bcs = create_dirichletbc(mesh, T_D)
 
+    # Assemble matrix A
     A = fem.petsc.assemble_matrix(a, bcs=bcs)
     A.assemble()
 
@@ -419,12 +450,22 @@ def thermal_solver(alpha):
     pc.setHYPREType("boomeramg")
 
     T = fem.Function(V)
-    plotter = plot_function(T, "room.gif")
+    T.interpolate(T0)
+    plotter = create_gif(T, "room.gif")
     plotter.write_frame()
 
+    # Check against standard table value
+    # Create bounding box for function evaluation
+    bb_tree = geometry.BoundingBoxTree(mesh, mesh.topology.dim)
+    p = numpy.array([0.5, 0.5, 0.0], dtype=numpy.float64)
+    cell_candidates = geometry.compute_collisions(bb_tree, p)
+    cells = geometry.compute_colliding_cells(mesh, cell_candidates, p)
+
+    value = T.eval(p, cells[0])
+    print(f"Inside temperature {value}")
 
     t = 0
-    t_max = 600
+    t_max = period * 2
     while t < t_max:
         t += dt.value
         print(f"t = {t/3600.0}")
@@ -445,12 +486,17 @@ def thermal_solver(alpha):
         solver.solve(b.vector, T.vector)
         T.x.scatter_forward()
 
+        value = T.eval(p, cells[0])
+        print(f"Inside temperature {value}")
+
         # Update un
         T0.x.array[:] = T.x.array
 
         # Update plotter
         plotter.update_scalars(T.x.array, render=False)
         plotter.write_frame()
+# -
 
-
+# +
 thermal_solver(alpha)
+# -
