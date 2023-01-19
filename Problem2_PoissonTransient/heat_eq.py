8ffd5de1ac69cb6082a230bc017f51d9adf50b8b
IgorBaratta <igorbaratta@gmail.com>
2023-01-19 18:01:00 +0000

    final version

--- Problem2_PoissonTransient/heat_eq.py
+++ Problem2_PoissonTransient/heat_eq.py
@@ -1,15 +1,33 @@
+# +
+try:
+  import dolfinx
+except ImportError:
+  !wget "https://github.com/fem-on-colab/fem-on-colab.github.io/raw/7f220b6/releases/fenicsx-install-real.sh" -O "/tmp/fenicsx-install.sh" && bash "/tmp/fenicsx-install.sh"
+  import dolfinx
+
+try: 
+  import pyvista
+except ImportError:
+  !pip install -q piglet pyvirtualdisplay ipyvtklink pyvista panel
+  !apt-get -qq install xvfb
+  import pyvista
+
+# Utils for plotting the mesh
+!wget https://raw.githubusercontent.com/IgorBaratta/FEniCSxCourse/main/Problem2_PoissonTransient/utils.py
+# -
+
 # # Solving a time-dependent problem
 
 #
-# This notebook shows how to solve a transient problem using DOLFINx, namely the diffusion problem,
-# the simplest extension of the Poisson problem.
+# This notebook shows how to solve a transient problem using DOLFINx, 
+# namely the diffusion problem, the simplest extension of the Poisson problem.
 #
 # The strong form of our problem reads:
 # $$
 # \begin{align*}
-# \frac{\partial T(\boldsymbol{x}, t)}{\partial t} - \nabla \cdot (\alpha  \nabla T(\boldsymbol{x}, t)) &= f(\boldsymbol{x}, t) & & \text{in } \, \Omega, \\
+# \frac{\partial T(\boldsymbol{x}, t)}{\partial t} - \nabla \cdot (\nabla T(\boldsymbol{x}, t)) 
+# &= f(\boldsymbol{x}, t) & & \text{in } \, \Omega, \\
 # T(\boldsymbol{x}, t) &= T_D(\boldsymbol{x}, t) & &\text{on} \,\partial\Omega_\text{D}, \\
-# \alpha\frac{\partial T}{\partial n} &= 0 & &\text{on} \, \partial\Omega_\text{N} \\
 # T(\boldsymbol{x}, t=0) &= T_i(\boldsymbol{x}) & & \text{in } \, \Omega,
 # \end{align*}
 # $$
@@ -21,68 +39,65 @@
 # To solve time-dependent PDEs using the finite element method, we first discretize the time derivative
 # using a finite difference approximation, yielding a recursive series of stationary problems,
 # and then we convert each stationary problem into a variational problem.
-#
-
 
 # ## Time discretization
 # A backward Euler scheme can be used to approximate the time derivative.
 # $$
 # \begin{align*}
-#   \frac{T_{n} - T_{n-1}}{\Delta t} - \nabla \cdot (\alpha  \nabla T_{n}) = f_{n}
+#   \frac{T_{n} - T_{n-1}}{\Delta t} - \nabla \cdot (\nabla T_{n}) = f_{n}
 # \end{align*}
 # $$
-# Reordering the last equation equation so that only unknows at time step n appear in the
-# left-hand side:
+# We may reorder the semi-discrete equation so that the left-hand side contains the terms with 
+# the unknown $T_{n}$ and the right-hand side contains computed terms only. 
 # $$
 # \begin{align*}
-#   T_{n} - \Delta t \nabla \, \cdot (\alpha  \nabla T_{n}) = \Delta t f_{n} + T_{n-1}
+#   T_{n} - \Delta t \nabla \, \cdot (\nabla T_{n}) = \Delta t f_{n} + T_{n-1}
 # \end{align*}
 # $$
 #
-# ## Time-stepping algorithm
-#       Compute T_0 as interpolation of a given function T_i(\boldsymbol{x})
-#       Define the bilinear a(T,v) and linear L(v) forms
-#       Assemble the matrix A from the bilinear form a
-#       t = 0
-#       while t < t_max:
-#           Assemble the vector b from the linear form L
-#           Apply time dependent boundary conditions
-#           Solve the linear system AT=b
-#           Update current solution T_n = T
-
 
 # ## Variational formulation
 # As usual we find the weak by multiplying our semi-discrete equation by a sufficiently
 # regular test function $v$ and applying integration by parts. At time-step $n$ the weak
-# reads: Find $u \in V(\Omega)$ such that
-#
+# reads: 
+# 
+# Find $u \in V(\Omega)$ such that
 # $$
 # \begin{align*}
-#   \int_{\Omega} T_n v \,dx + \Delta t \int_{\Omega}{ \alpha \nabla T_n \cdot \nabla v}\,dx = \Delta t \int_{\Omega} f \cdot v \, dx + \int_{\Omega} T_0 \cdot v \, dx
+#   \int_{\Omega} T_n v \,dx + \Delta t 
+#   \int_{\Omega}{ \alpha \nabla T_n \cdot \nabla v}\,dx 
+#   = \Delta t \int_{\Omega} f \cdot v \, dx + \int_{\Omega} 
+#   T_{n-1} \cdot v \, dx
 # \end{align*}
 # $$
-# For this problem, the natural choice is a space of continuous functions, that are element-wise polynomials of degree $k$
-#
+
+# For this problem, the natural choice is a space of scalar-valued continuous functions, 
+# that are element-wise polynomials of degree $k$
 # $$
-# V(\mathcal{T}_h) = V_h = \{v \in H ^ 1(\Omega), ~v | _E \in P_k(E) \, \forall E \in \mathcal{T}_h\}
+# V(\mathcal{T}_h) = V_h = \{v \in H ^ 1(\Omega), 
+# ~v | _E \in P_k(E) \, \forall E \in \mathcal{T}_h\}
 # $$
 
 # ## Test problem - Manufactured Solution
-# We construct a test problem for which we can easily check the answer. We first define the exact solution by
+# We construct a test problem for which we can easily check the answer. 
+# We create a test problem with a linear variation in time because we know 
+# our first-order time-stepping scheme is exact for linear functions.
 #
 # $$
 # \begin{align*}
 #   T(\boldsymbol{x}, t) = c_0 x_0^2 + c_1 x_0 + c_3t
 # \end{align*}
 # $$
+# which produces a function whose computed values at the nodes are exact regardless 
+# of element size and $\delta t$, as long as the mesh is partitioned uniformly.
 # and then we insert the exact solution into the strong form of our pde.
 # With simple manipulations we realize that the source should be:
 # $$
 # \begin{align*}
-#   f = c_4 - 2c_0
+#   f = c_2 - 2c_0
 # \end{align*}
 # $$
-# Also at the boundaries we have:
+# Also, at the boundaries we have:
 # $$
 # \begin{align*}
 #   T_D(\boldsymbol{x}, t) = c_0 x_0^2 + c_1 x_0 + c_2t
@@ -90,10 +105,15 @@
 # $$
 
 # ## Implementation
-
+#
 # The first step is to import all modules and packages we are going to use:
 
 # +
+# Auxiliary packages
+from petsc4py import PETSc # Linear algebra backend
+from mpi4py import MPI     # MPI 
+import numpy
+
 # Import dolfinx and ufl modules
 from dolfinx import geometry
 from dolfinx import fem
@@ -102,13 +122,8 @@ from dolfinx import la
 from dolfinx.mesh import (CellType, create_unit_square, locate_entities, exterior_facet_indices)
 from ufl import TestFunction, TrialFunction, dx, inner, grad
 
-# Auxiliary packages
-from petsc4py import PETSc
-from mpi4py import MPI
-import numpy
-
 # Convenience functions for plotting on jupyter notebooks
-from utils import plot_mesh, plot_function, create_gif
+from utils import plot_mesh, create_gif
 import IPython
 # -
 
@@ -120,7 +135,7 @@ import IPython
 
 # +
 comm = MPI.COMM_WORLD
-mesh = create_unit_square(comm, 10, 10, CellType.quadrilateral)
+mesh = create_unit_square(comm, 10, 10, CellType.triangle)
 # -
 
 
@@ -137,9 +152,7 @@ IPython.display.HTML(filename="mesh.html")
 # First we define the constants we use in our program:
 
 # +
-c0 = fem.Constant(mesh, 1.0)
-c1 = fem.Constant(mesh, 2.0)
-c2 = fem.Constant(mesh, 0.5)
+c0, c1, c2 = 1.0, 2.0, 0.5
 dt = fem.Constant(mesh, 0.1)
 # -
 
@@ -159,16 +172,19 @@ print(type(f))
 # it into the corresponding function space.
 
 # +
-def expression(t):
-    return lambda x: c0*x[0]**2 + c1*x[0] + c2*t
+from functools import partial 
+
+def expression(t, x):
+    return c0*x[0]**2 + c1*x[0] + c2*t
 
 V = fem.FunctionSpace(mesh, ("Lagrange", 1))
 T0 = fem.Function(V)
-T0.interpolate(expression(t=0))
+T0.interpolate(partial(expression, 0))
 
-# plot T0 over Omega
-plot_function(T0, filename="T0.html")
-IPython.display.HTML(filename="T0.html")
+T0.name = "Temperature0"
+with io.XDMFFile(MPI.COMM_WORLD, "t0.xdmf", "w") as xdmf:
+    xdmf.write_mesh(mesh)
+    xdmf.write_function(T0)
 # -
 
 # ## Setting up a variational problem
@@ -188,8 +204,7 @@ T0 = fem.Function(V)
 # $$
 
 # +
-alpha = fem.Constant(mesh, 1.0)
-a = inner(T, v) * dx + dt * alpha * inner(grad(T), grad(v)) * dx
+a = inner(T, v) * dx + dt * inner(grad(T), grad(v)) * dx
 a = fem.form(a)  # JIT compilation
 # -
 
@@ -212,28 +227,34 @@ L = dt * inner(f, v) * dx + inner(T0, v) * dx
 # +
 # Create Dirichlet function
 T_D = fem.Function(V)
-T_D.interpolate(expression(0))
+T_D.interpolate(partial(expression, 0))
 
 # Define Dirichlet bc
+
+# Get boundary facets
 tdim = mesh.topology.dim
 mesh.topology.create_connectivity(tdim - 1, tdim)
 bndry_facets = exterior_facet_indices(mesh.topology)
+
+# Locate degrees of freedom on those facets
 bndry_dofs = fem.locate_dofs_topological(V, tdim - 1, bndry_facets)
+
+# Create list of Dirichlet BC
 bcs = [fem.dirichletbc(T_D, bndry_dofs)]
 # -
 
 # ### Setting up a time dependent solver
 #
-# As the left hand side of our problem(the matrix) is time independent, we would like avoid re-assembling it at every time step.
-# We assemble the matrix once outside the temporal loop.
+# As the left hand side of our problem(the matrix) is time independent, we would like avoid 
+# re-assembling it at every time step.We assemble the matrix once outside the temporal loop.
 
 # +
 A = fem.petsc.assemble_matrix(a, bcs=bcs)
 A.assemble()
 # -
 
-# Next, we can generate the integration kernel for the right hand side(RHS), and create the RHS vector `b` that we will
-# assembled into at each time step.
+# Next, we can generate the integration kernel for the right hand side(RHS), 
+# and create the RHS vector `b` that we will assembled into at each time step.
 
 # +
 b = fem.Function(V)
@@ -255,15 +276,14 @@ pc.setHYPREType("boomeramg")
 
 # ### Plotting a time dependent problem
 
-# As we are solving a time dependent problem, we would like to create a time dependent animation of the solution.
-# We do this by using [pyvista](https: // docs.pyvista.org /), which uses VTK structures for plotting.
-# In DOLFINx, we have the convenience function `dolfinx.plot.create_vtk_mesh` that can create meshes compatible
-# with VTK formatting, based on meshes of(discontinuous) Lagrange function spaces.
+# As we are solving a time dependent problem, we would like to create a time dependent animation 
+# of the solution. The first step is to create an output file where we are going to save the 
+# solution at each time step:
 
 # +
-T = fem.Function(V)
-plotter = create_gif(T, "T.gif", [0, 2])
-plotter.write_frame()
+# Output file
+file = io.XDMFFile(MPI.COMM_WORLD, "temperature.xdmf", "w")
+file.write_mesh(mesh)
 # -
 
 
@@ -277,14 +297,17 @@ plotter.write_frame()
 # 5. Update current solution, `T0 = T`
 
 # +
+T = fem.Function(V)
+T.name = "temperature"
+
 t = 0
-t_max = 10*dt.value
+t_max = 100*dt.value
 while t < t_max:
     t += dt.value
     print(f"t = {t:.2f} s")
 
     # Update boundary condition
-    T_D.interpolate(expression(t))
+    T_D.interpolate(partial(expression, t))
 
     # Assemble RHS
     b.x.array[:] = 0
@@ -303,12 +326,10 @@ while t < t_max:
     # Update un
     T0.x.array[:] = T.x.array
 
-    # Update plotter
-    plotter.update_scalars(T.x.array, render=False)
-    plotter.write_frame()
+    # Save solution at time step t
+    file.write_function(T, t=t)
 
-
-IPython.display.Image(filename="T.gif")
+file.close()
 # -
 
 # What's the temperature at an arbitrary point p?
@@ -327,19 +348,17 @@ values = T.eval(p, cells[0])
 print(f"Temperature at point {p} is {values[0]}")
 # -
 
-# **TASK 1**: compute the L2 and H1 error norms
+# **TASK 1**: The above time-stepping loop does not include any 
+# comparison of numerical and exact solutions, which is required 
+# to validate the implementation. In the previous section we've 
+# learned how to compute the L2 and H1 error norms.
+#
+# Use the knowledge gained in the previous section to calculate both
+# the L2 and H1 error norms for $t=t_{max}$.
 
 # +
-eh = T_D - T
-M = fem.form(eh**2 * dx)
-error_norm =fem.assemble_scalar(M)
-print(f"L2 Error norm: {error_norm}")
+# Solution goes here:
 # -
-
-# **TASK 2**: Change the cell type and polynomial degree.
-# Increase and decrease the time step dt.
-# Why doesn't the norm of error change?
-
 
 # # Homework 1
 # In this homework we are going to tackle a more physically realistic problem.
@@ -349,30 +368,37 @@ print(f"L2 Error norm: {error_norm}")
 # Without internal sources the strong form of our problem reads:
 # $$
 # \begin{align*}
-# \frac{\partial T}{\partial t} &= \nabla \cdot (\alpha  \nabla T) & & \text{in } \, \Omega, \\
+# \rho C_p\frac{\partial T}{\partial t} &= \nabla \cdot (\kappa  \nabla T) & & \text{in } \, \Omega, \\
 # T(\boldsymbol{x}, t) &= T_D = T_R + T_a \cdot sin(\omega t) & & \,\partial\Omega_\text{D}, \\
 # T(\boldsymbol{x}, t=0) &= T_0 & & \text{in } \, \Omega,
 # \end{align*}
 # $$
-# Where $\alpha$ is the thermal diffusivity of the medium:
+# where $k$ is the thermal conductivity, $\rho$ is the density and $C_p$ is the 
+# is specific heat capacity.
+#
+# In this example, we assume computational domain is divided into 
+# two domains $\Omega_{air}$ and $\Omega_{wall}$. Within each domain
+# we can define the thermal diffusivity of the medium $\alpha$:
 # $$
 # \begin{align*}
 # \alpha = \frac{k}{\rho C_p}
 # \end{align*}
 # $$
-# $k$ is the thermal conductivity, $\rho$ is the density and $C_p$ is the is specific heat capacity.
+# 
 # Examples of thermal diffusivity for four different materials:
 # $$\alpha_{air} = 19 \cdot 10^{-6} \, m^2/s$$
+# $$\alpha_{glass} = 0.34	 \cdot 10^{-6} \, m^2/s$$
 # $$\alpha_{brick} = 0.27 \cdot 10^{-6} \, m^2/s$$
 # $$\alpha_{wood} = 0.082 \cdot 10^{-6} \, m^2/s$$
 # $$\alpha_{steel} = 11.72 \cdot 10^{-6} \, m^2/s$$
 #
-# Given a 1 squared meter room, 0.1 m thick walls, and with outside 
-# temperature given by $T_D$ which material should be used for the wall 
-# such that the temperature at the center of the room is never below $10^oC$?
+# Given unit square room $[1m \times 1m]$, $0.1 m$ thick walls, and outside 
+# temperature given by $T_D$, which material should be used for the wall 
+# such that the temperature at the center of the room never drops below 
+# $10^oC$?
 
-# **TASK 1**: Complete the following code with the thermal diffusivity of the air and
-# the wall and visualize the material distribution:
+# **TASK 1**: Complete following code with the diffusivity of the wall 
+# (chosen from the above values). Then visualize the material distribution.
 
 # +
 def wall(x):
@@ -389,12 +415,10 @@ DG = fem.FunctionSpace(mesh, ("DG", 0))
 
 # Thermal diffusivity
 alpha = fem.Function(DG)
-# alpha.x.array[:] = alpha_air
-# alpha.x.array[cells] = alpha_wall
-
-# Solution - 
 alpha.x.array[:] = 19e-6
-alpha.x.array[cells] = 0.082e-6 # wood
+
+# Edit here: diffusivity of wall
+alpha.x.array[cells] = 0.37e-6 # wood
 
 plot_mesh(mesh, cell_values=alpha, filename="alpha.html")
 IPython.display.HTML(filename="alpha.html")
@@ -403,37 +427,63 @@ IPython.display.HTML(filename="alpha.html")
 
 # **Task 2**: create a python function that defines the outside temperature
 # $$T_{out} = -5 + 15 \cdot sin(\omega t)$$
-# where $\omega = \pi/(24\cdot60\cdot60)$.
+# where $\omega = \pi/(24\cdot60\cdot60)$. 
+# Plot the temperature for a period of 24 hours.
 
 # +
 period = 60*60*24 # seconds in a day
+def outside_temperature(t, x):
+    # Edit this function
+    temperature = 0
+    return numpy.full_like(x[0], temperature)
 
-# Task: define temperature outside temperature and plot it for 
-def outside_temperature(t):
+# Task: Plot the hourly temperature during a day:
+temp = numpy.zeros(24)
+time = numpy.zeros(24)
+for i in range(temp.size):
+    time[i] = i*60*60
+    temp[i] = outside_temperature(time[i], [0, 0])
+
+import matplotlib.pyplot as plt
+plt.plot(time, temp)
+plt.xlabel('Time (hour)', fontsize=20)
+plt.ylabel('Temperature ($^oC$)', fontsize=20)
+plt.savefig("temperature.png")
+
+# 
+# -
+
+# +
+# Reference solution
+period = 60*60*24 # seconds in a day
+
+def outside_temperature(t, x):
     omega = numpy.pi/period
     t = t % period
     temperature = 15 * numpy.sin(omega*t) - 5
-    print(f"Outside temperature = {temperature}")
-    return lambda x: numpy.full_like(x[0], temperature)
+    return numpy.full_like(x[0], temperature)
 
-# Task: Plot the hourly temperature during a day:?
+# Task: Plot the hourly temperature during a day:
 temp = numpy.zeros(24)
+time = numpy.zeros(24)
 for i in range(temp.size):
-    t = i*0.5*60*60
-    temp[i] = outside_temperature(t)([0,0])
+    t = i*60*60
+    time[i] = i
+    temp[i] = outside_temperature(t, [0, 0])
 
 import matplotlib.pyplot as plt
-
-plt.plot(temp)
-plt.savefig("temperature.png")
-IPython.display.Image(filename="temperature.png")
+plt.plot(time, temp)
+plt.xlabel('Time (hour)', fontsize=20)
+plt.ylabel('Temperature ($^oC$)', fontsize=20)
 # -
 
-# **Task 3** Modify the following function to return the hourly variation 
-# of temperature inside the room.
+
+# **Task 3**: Modify the following thermal solver so that it returns 
+# the hourly temperature inside the room at each time step.
+# Save the solution at each time-step and visualize with paraview.
 
 # +
-def thermal_solver(alpha):
+def thermal_solver(alpha, intial_temp=15, time_step=900.0):
     mesh = alpha.function_space.mesh
 
     # Create function space
@@ -445,10 +495,10 @@ def thermal_solver(alpha):
 
     # Initial condition
     T0 = fem.Function(V)
-    T0.interpolate(lambda x: numpy.full_like(x[0], 12))
+    T0.interpolate(lambda x: numpy.full_like(x[0], intial_temp))
 
     # Time step (15 min - 900 seconds)
-    dt = fem.Constant(mesh, 900.0)
+    dt = fem.Constant(mesh, time_step)
 
     # Bilinear form
     a = inner(T, v) * dx + dt * alpha * inner(grad(T), grad(v)) * dx
@@ -460,19 +510,20 @@ def thermal_solver(alpha):
 
 
     # Define dirichlet bc at t=0
+    temp0 = partial(outside_temperature, 0)
     T_D = fem.Function(V)
-    T_D.interpolate(outside_temperature(0))
+    T_D.interpolate(temp0)
     
+    # Create boundary condition
     tdim = mesh.topology.dim
     mesh.topology.create_connectivity(tdim - 1, tdim)
     bndry_facets = exterior_facet_indices(mesh.topology)
     bndry_dofs = fem.locate_dofs_topological(V, tdim - 1, bndry_facets)
     bcs = [fem.dirichletbc(T_D, bndry_dofs)]
 
-    # Assemble matrix A
+    # Assemble matrix A and RHS b
     A = fem.petsc.assemble_matrix(a, bcs=bcs)
     A.assemble()
-
     b = fem.Function(V)
 
     solver = PETSc.KSP().create(comm)
@@ -484,8 +535,6 @@ def thermal_solver(alpha):
 
     T = fem.Function(V)
     T.interpolate(T0)
-    plotter = create_gif(T, "room.gif", clim=[-5, 15])
-    plotter.write_frame()
 
     # Check against standard table value
     # Create bounding box for function evaluation
@@ -495,21 +544,22 @@ def thermal_solver(alpha):
     cells = geometry.compute_colliding_cells(mesh, cell_candidates, p)
 
     value = T.eval(p, cells[0])
-    print(f"Inside temperature {value}")
-
+    print(f"Room temperature {value}")
 
     t = 0
-    t_max = period * 1
+    num_days = 1
+    t_max = period * num_days
 
     # Array to store temperature
-    temperature = numpy.zeros(int(t_max/dt.value))
+    temperature = [value]
+    time = [t]
 
     while t < t_max:
+        time.append(t)
         t += dt.value
-        print(f"t = {t/3600.0} h")
 
         # Update boundary condition
-        T_D.interpolate(outside_temperature(t))
+        T_D.interpolate(partial(outside_temperature, t))
 
         # Assemble RHS
         b.x.array[:] = 0
@@ -525,22 +575,19 @@ def thermal_solver(alpha):
         T.x.scatter_forward()
 
         value = T.eval(p, cells[0])
-        print(f"Inside temperature {value}")
+        print(f"Room temperature {value[0]} at {t/(60*60)} h")
 
         # Update un
         T0.x.array[:] = T.x.array
-
-        # Update plotter
-        plotter.update_scalars(T.x.array, render=False)
-        plotter.write_frame()
     
-    return temperature
+    return temperature, time
 # -
 
 # **Task 4**: Which material should we use for insulation?
-# Plot the T vs time for different types of material for the wall.
+# Modify the alpha value on the wall and plot temperature
+# vs time inside the room.
 
 # +
-alpha.x.array[cells] = 0.082e-6 # wood
-temp = thermal_solver(alpha)
+temperature, time = thermal_solver(alpha)
+assert len(temperature) == len(time)
 # -
